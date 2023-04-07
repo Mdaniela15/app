30f42e30bf7778c144aef40e999ea688fbb2c97e
mdaniela15 <mdup98@gmail.com>
2023-04-07 19:47:27 -0300

    componentes

---
+++ src/componentes/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+        <p className="footer"__text>Copyrigth & copy</p>
+        </footer>
+    )
+}
+export default Footer
---
+++ src/componentes/Header.js
@@ -0,0 +1,17 @@
+
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Titulo</h1>
+      <nav className="header__nav">
+        <a href="#" className="header__link link">link</a>
+        <a href="#" className="header__link link">link</a>
+        <a href="#" className="header__link link">link</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
