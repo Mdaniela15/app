5f355f70156859f9e507a4baadd7315c4a1e7ad9
mdaniela15 <mdup98@gmail.com>
2023-04-08 23:45:41 -0300

    header

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -4,7 +4,7 @@
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header__title">Azúcar y Canela</h1>
+      <h1 className="header__title">Azúinscar y Canela</h1>
       <nav className="header__nav">
         <a href="#" className="header__link link">Productos</a>
         <a href="#" className="header__link link">Nosotros</a>
